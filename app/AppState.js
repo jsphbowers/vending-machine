b9a2aba06da65ba2a3389ad445e4cc6dee54a7a3
jsphbowers <jsphbowers@gmail.com>
2023-03-07 13:56:00 -0700

    Initial Set up

--- app/AppState.js
+++ app/AppState.js
@@ -1,11 +1,19 @@
+import { Snack, snack } from "./Models/Snack.js"
 import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 import { loadState } from "./Utils/Store.js"
 
 class AppState extends EventEmitter {
-  /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', [Value])
+  // /** @type {import('./Models/Value').Value[]} */
+  // values = loadState('values', [Value])
+
+  snacks = [
+    new Snack({ name: 'candy-bar', price: 2, imgUrl: 'https://images.unsplash.com/photo-1621939514649-280e2ee25f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c25hY2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' })
+  ]
+
+  money = 0
+
 }
 
 export const appState = new Proxy(new AppState(), {
